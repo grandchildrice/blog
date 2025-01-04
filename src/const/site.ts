5ae010e571d4e830645b265bddb52393c8b28944
masatsch <kingmasatojames@gmail.com>
2025-01-04 18:44:23 +0900

    blog title

--- src/const/site.ts
+++ src/const/site.ts
@@ -1,10 +1,10 @@
 import { Env } from './env';
 
 export const Site = {
-  title: 'n2zm Blog',
-  description: 'Demo Blog using Next.js, Notion, and notion-to-zenn-md',
+  title: 'gohan blog',
+  description: 'internet inventor',
   url: Env.BaseUrl,
   locale: 'en_US',
-  icon: '🌵',
+  icon: '🍚',
   userName: '',
 } as const;
